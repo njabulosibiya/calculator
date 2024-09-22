cc4c59868ba8665fcba5b8bb320f262334eaeda2
Njabulo Sibiya <75039043+njabulosibiya@users.noreply.github.com>
2024-09-22 20:37:27 +0200

    chore: initial code commit
    Unlike the initial commit that adds empty files and folders to get a
    general file structure of the project, this commit adds function code
    changes.
    
    * Added fmt library dependency through vcpkg. Fmt library to be used
      as standard text stream, replacing iostream
    * Updated CMake relative to new dependency.
    * Created new main.cpp files for the targets to be generated. These
      targets are: UI, Console & main. Name is reflective of its function.
    * renamed include/include.h to include/core.h
    * Updated ABOUT_DESIGN.txt

---
+++ console_main.cpp
@@ -0,0 +1,30 @@
+/* File:	console_main.cpp
+ * 
+ * Project:	Calculator
+ * 
+ * Author:	Njabulo Sibiya (75039043+njabulosibiya@users.noreply.github.com)
+ * 
+ * Date:	22 September 2024
+ * 
+ * Dependencies:	fmt
+ * 
+ * Description:
+ *	The main executable of the terminal version of the Calculator application.
+ *	
+ *	The terminal version is strictly for development of the back-end logic of 
+ *	the calculator application, building the algorithms & code that facilitate
+ *	the functionality of the calculator, debugging & experimental features.
+ *	
+ *	In essence this compiles the core part of the calculator (Excluding 
+ *	library), without any UI.
+ */
+
+#include <fmt/core.h>
+#include <iostream>
+#include <core.h>
+
+int main() {
+	fmt::print("Hello FMT");
+
+	return 0;
+}
---
+++ include/core.h
@@ -0,0 +1,11 @@
+#ifndef CORE_H
+#define CORE_H
+
+namespace nazcal {
+#define PI = 3.14;
+	constexpr double getPI() {
+		return 3.14;
+	}
+}
+
+#endif // CORE_H
